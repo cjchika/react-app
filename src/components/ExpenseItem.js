41429c520be3f8f8836ed19cb61880d18b8912b7
Chijioke Chika <8pluscreative@gmail.com>
2021-12-29 05:30:44 -0800

    Adding files

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', { month: 'long'});
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
     return (
        <div className="expense-item">
-            <div>March 20 2022</div> 
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div> 
             <div className="expense-item__description">
-                <h2>Car Maintainance</h2>
-                <div className="expense-item__price">$500.56</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>  
         </div> 
     )
