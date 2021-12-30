3038edc4e29aef0aceea8e5a9256a42ab5446822
Chijioke Chika <8pluscreative@gmail.com>
2021-12-30 10:21:18 -0800

    updating

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
 
 function Expenses(props) {
+	const [filteredYear, setFilteredYear] = useState('2019');
+
+	const filterChangeHandler = selectedYear => {
+		setFilteredYear(selectedYear);
+	};
+	
 	return (
+		<div>
 		<Card className="expenses">
+			<ExpensesFilter selected={filteredYear} onChangeYear={filterChangeHandler} />
 			<ExpenseItem
 				title={props.items[0].title}
 				amount={props.items[0].amount}
@@ -28,6 +37,7 @@ function Expenses(props) {
 				date={props.items[3].date}
 			/>
 		</Card>
+		</div>
 	);
 }
 
